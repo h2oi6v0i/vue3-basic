89e1cb9017c96c65f86a219b7a915a157cc0386c
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-03 00:54:31 +0900

    [11] : Template Syntax

---
+++ src/lectures/App_TemplateSyntax.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>속성 바인딩</h3>
+    <div :title="dynamicTitle">마우스를 올려보세요🖱</div>
+    <input type="text" value="하이" :disabled="isInputDisabled" />
+    <input v-bind="attrs" />
+
+    <h3>JavaScript</h3>
+    <div>{{ message.split('').reverse().join('') }}</div>
+    <div>{{ isInputDisabled ? 'YES' : 'NO' }}</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('안녕하세요!');
+    const dynamicTitle = ref('🍀');
+    const isInputDisabled = ref(true);
+
+    /** 다중 속성 바인딩 */
+    const attrs = ref({
+      type: 'password',
+      value: '12345678',
+      disabled: false,
+    });
+
+    return {
+      message,
+      dynamicTitle,
+      isInputDisabled,
+      attrs,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
