68660a36e7f5aa1d19e50ecb6b517e25758908f0
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-03 00:05:54 +0900

    [7] : ESLint, Prettier 설정 및 파일 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,31 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // 운영일 때는 에러 표시, 개발 중엔 허용
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto', // 한줄 추가
+      },
+    ],
+  },
+};
