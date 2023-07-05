37ab6fc186fcc98f15c56f83a5ddca5a81b6d8b0
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-05 23:41:45 +0900

    [14] : Class, Style 바인딩

---
+++ src/lectures/App_class_binding.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div
+      :class="[isActive ? 'active-class' : 'class', activeClass, errorClass]"
+    >
+      텍스트 📝
+    </div>
+    <button @click="toggle">토글토글</button>
+    <button @click="hasError = !hasError">에러 발생</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  setup() {
+    const isActive = ref(true);
+    const hasError = ref(false);
+
+    // const classObject = ref({
+    //   active: true,
+    //   'text-danger': true,
+    // });
+
+    const classObject = computed(() => {
+      return {
+        active: true && true,
+        'text-danger': true && true,
+        'text-blue': true,
+      };
+    });
+
+    const activeClass = ref('active');
+    const errorClass = ref('error');
+
+    const toggle = () => {
+      isActive.value = !isActive.value;
+    };
+
+    return { isActive, toggle, hasError, classObject, activeClass, errorClass };
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  font-weight: 900;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
---
+++ src/lectures/App_style_binding.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div :style="styleObject">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam aliquam
+      illo fugiat deserunt amet exercitationem animi cupiditate, ut nihil
+      quaerat quidem dolor beatae aspernatur! Mollitia temporibus odit
+      doloremque voluptatum illum?
+    </div>
+    <button @click="fontSize--">-</button>
+    <button @click="fontSize++">+</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+export default {
+  setup() {
+    // const styleObject = reactive({
+    //   color: 'cornflowerblue',
+    //   fontSize: '18px',
+    // });
+
+    const fontSize = ref(18);
+
+    const styleObject = computed(() => {
+      return {
+        color: 'cornflowerblue',
+        fontSize: fontSize.value + 'px',
+      };
+    });
+
+    return { styleObject, fontSize };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
