7cd46db1bc1fdaba6990dee3e24d2d4a41c80ca1
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-18 00:04:53 +0900

    🚚 폴더 구조 변경

--- src/lectures/v-if/App_v-if.vue
+++ src/lectures/v-if/App_v-if.vue
@@ -15,12 +15,6 @@
     <button @click="type = 'C'">C 버튼</button>
     <button @click="type = 'D'">D 버튼</button>
 
-    <p>
-      한가지 팁을 추가하자면, 여러개의 태그를 v-if 조건문에 따라 보여지거나 사라지도록 할때,
-      &lt;div&gt;태그를 wrapper로 사용하기 보다는 새로운 &lt;template&gt; 태그를 사용해 여러개의
-      태그를 감싸고 v-if 조건문을 달아주면,&nbsp; wrapper로 사용된 &lt;template&gt; 태그는 렌더링
-      되지 않는 이점이 있습니다.&nbsp;
-    </p>
     <template v-if="visible">
       <h1>Title</h1>
       <p>Paragraph 1</p>
