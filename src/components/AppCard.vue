fea328576755098ec5f1f086cbdd999ec5099d1f
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-30 22:16:19 +0900

    [24] : props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,37 +1,70 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title red">Card title</h5>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <span class="badge bg-secondary"> {{ typeName }}</span>
+      <h5 class="card-title mt-2">{{ title }}</h5>
+      <p class="card-text">{{ contents }}</p>
+      <a href="#" class="btn" :class="isLikeClass" @click="toggleLike">좋아요</a>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-// import { useCssModule } from 'vue';
+import { computed } from 'vue';
 
 export default {
-  setup() {
-    // const style = useCssModule();
-    // console.log(style);
+  props: {
+    type: {
+      type: String,
+      default: 'news',
+      validator: (value) => {
+        return ['news', 'notice'].includes(value);
+      },
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    contents: {
+      type: String,
+      required: true,
+    },
+    isLike: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 배열, 객체같은 레퍼런스 타입의 기본값은 팩토리 함수에서 반환 되어야 한다.
+     * data를 함수로 선언해서 return 하는 것과 같은 이유이다.
+     * reference 타입의 자료형들은 함수로 새롭게 만들어서 리턴을 안 해주면 원본을 참조, 공유해버리기 때문에 함수로 만들어 리턴을 하는 방식으로 해야 하는 것 같다.
+     */
+    obj: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
 
-    const color = ref('red');
+  emits: ['toggleLike'],
 
-    return { color };
+  setup(props, context) {
+    /** 카드 타입 */
+    const typeName = computed(() => {
+      return props.type === 'news' ? 'News' : 'Notice';
+    });
+
+    /** 좋아요 상태별 클래스 */
+    const isLikeClass = computed(() => {
+      return props.isLike ? 'btn-danger' : 'btn-outline-danger';
+    });
+
+    /** 좋아요 상태 토글 */
+    const toggleLike = () => {
+      context.emit('toggleLike');
+    };
+
+    return { isLikeClass, typeName, toggleLike };
   },
 };
 </script>
-
-<style>
-/* <style module> : style에 module 속성을 붙이면 스타일 규칙이 $style 내장 객체 속성으로 포함된다. */
-/* <style module="classes"> : classes */
-.red {
-  /* color: red !important; */
-  color: v-bind(color) !important;
-}
-</style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -2,8 +2,14 @@
   <main>
     <div class="container py-4">
       <div class="row g-3">
-        <div v-for="item in 6" :key="item" class="col col-4">
-          <AppCard></AppCard>
+        <div v-for="post in posts" :key="post.id" class="col col-4">
+          <AppCard
+            :type="post.type"
+            :title="post.title"
+            :contents="post.contents"
+            :isLike="post.isLike"
+            @toggle-like="post.isLike = !post.isLike"
+          />
         </div>
       </div>
     </div>
@@ -11,14 +17,24 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
 import AppCard from '@/components/AppCard.vue';
 
 export default {
   components: {
     AppCard,
   },
+
   setup() {
-    return {};
+    const posts = reactive([
+      { id: 1, type: 'news', title: '제목1', contents: '내용1', isLike: true },
+      { id: 2, type: 'news', title: '제목2', contents: '내용2', isLike: false },
+      { id: 3, type: 'notice', title: '제목3', contents: '내용3', isLike: true },
+      { id: 4, type: 'notice', title: '제목4', contents: '내용4', isLike: false },
+      { id: 5, type: 'news', title: '제목5', contents: '내용5', isLike: true },
+    ]);
+
+    return { posts };
   },
 };
 </script>
