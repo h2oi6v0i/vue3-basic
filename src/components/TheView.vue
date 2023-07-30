02e675ff21ce171d966b89672f83157b2fc14bb6
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-30 20:49:58 +0900

    [22] : 컴포넌트 생성 - TheView, TheNav, AppCard

---
+++ src/components/TheView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <div class="row g-3">
+        <div v-for="item in 6" :key="item" class="col col-4">
+          <AppCard></AppCard>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import AppCard from '@/components/AppCard.vue';
+
+export default {
+  components: {
+    AppCard,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+// import AppCard from '@/components/AppCard.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+// app.component('Appcard', AppCard); // 컴포넌트 전역 등록
+app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
