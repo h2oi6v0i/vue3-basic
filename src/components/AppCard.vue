bc4f383b11ac7ae94a7d0428f21c7586382e3d0c
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-07-30 21:03:37 +0900

    [23] : CSS 모듈

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title red">Card title</h5>
       <p class="card-text">
         Some quick example text to build on the card title and make up the bulk of the card's
         content.
@@ -12,11 +12,26 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+// import { useCssModule } from 'vue';
+
 export default {
   setup() {
-    return {};
+    // const style = useCssModule();
+    // console.log(style);
+
+    const color = ref('red');
+
+    return { color };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+/* <style module> : style에 module 속성을 붙이면 스타일 규칙이 $style 내장 객체 속성으로 포함된다. */
+/* <style module="classes"> : classes */
+.red {
+  /* color: red !important; */
+  color: v-bind(color) !important;
+}
+</style>
